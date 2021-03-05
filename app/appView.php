fd7cdf2a4ef6fe96b002ef71414fa8342cf526e6
bwpi <bwpi@yandex.ru>
2021-03-05 14:12:29 +0500

    m2

--- app/appView.php
+++ app/appView.php
@@ -11,21 +11,6 @@ function getView($route, $data = [], $types = '.html', $layout = 'default.php') 
     return $content = ob_get_clean();    
 }
 
-// function outVariant($data, $template, $classes = 'btn btn-primary mx-1', $delimetr = '/') {
-//     $out = explode($delimetr, $data);
-//     if ($template) {
-//         foreach ($out as $value) {
-//             $temp .= $template($value, $classes);
-//         }
-//         return $temp;
-//     }
-//     return $out;
-// }
-
-// function outResponseOptions($data, $classes) {
-//     return "<a class='{$classes}'>{$data}</a>";
-// }
-
 function outVariant($data, $template, $classes = 'btn btn-primary mx-1', $delimetr = '/') {    
     $out = explode($delimetr, $data);
     if ($template) {        
@@ -34,7 +19,7 @@ function outVariant($data, $template, $classes = 'btn btn-primary mx-1', $delime
     return $out;
 }
 
-function outResponseOptions($in, $classes) {    
+function multiResponseOptions($in, $classes) {    
     foreach ($in as $value) {
         $template .= "<a class='{$classes}'>{$value}</a>";
     }
